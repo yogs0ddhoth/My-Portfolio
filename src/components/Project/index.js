7afec6fdf4983ca06b1689a5ef4e717974c7aa17
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-31 15:26:24 -0400

    component copied and modularized

---
+++ src/components/Project/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../assets/css/index.css';
+// import '../../assets/css/style.css';
+import '../../assets/css/Project.css';
+
+export default function Project(props) {
+  return (
+    <div className='project col-12 col-md-5'>
+      <a href={props.deployedApp} className=''>
+        <img src={props.img} alt={props.description} />
+      </a>
+      <div>
+        <h4>{props.name}</h4>
+        <p>{props.description}</p>
+        <p>{props.technologies}</p>
+        <a href={props.repoLink}>Link to Repository</a>
+      </div>
+    </div>
+  )
+};
