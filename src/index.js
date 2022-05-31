cc0ceff10b83a265dea95532bb73c2a0417e6043
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-31 15:11:36 -0400

    react app created

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { HashRouter as Router } from 'react-router-dom';
+
+import './assets/css/index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>
+);
