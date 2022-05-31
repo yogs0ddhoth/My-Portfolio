73405b33ac54c8a3ec1a7c6e1a9a1c982348a307
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-31 15:28:22 -0400

    copied from react-portfolio template, data updated

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import './assets/css/index.css';
+import './assets/css/style.css';
+import './assets/css/App.css';
+
+import Header from './components/Header';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import Footer from './components/Footer';
+
+function App() {
+
+  return (
+    <div className="App theme-1">
+      <div className='contentWrap'>
+        <Header />
+
+        <Routes>
+          <Route path='/' element={<About />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/resume' element={<Resume />} />
+        </Routes>
+      </div>
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Resume.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+
+import benLinResume from '../assets/resume/Ben-Lin-Resume.pdf';
+import '../assets/css/index.css';
+import '../assets/css/style.css';
+
+export default function Resume() {
+  
+  return (
+    <div className='col-12 flex-row'>
+      <h2 className='title'>
+        My Resume:<a href={benLinResume} download={Resume} className='title col-12 align-start'>
+        {/* <FontAwesomeIcon icon={solid('p')} /> */}
+        download
+        </a>
+      </h2>
+      
+      <div className='title col-12 flex-row align-start'>
+        <h3 className='col-12'>Proficiencies:</h3>
+        <ul className='col-12 '>
+          <li>sed do</li>
+          <li>eiusmod</li>
+          <li>tempor incididunt</li>
+          <li>ut labore</li>
+          <li>et dolore</li>
+          <li>magna aliqua</li>
+        </ul>
+      </div>
+    </div>
+  )
+};
