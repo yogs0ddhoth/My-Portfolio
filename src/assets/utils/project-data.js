d042bc9b4598d546e353300d582080747d1c29c0
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-31 15:23:25 -0400

    project data stored here to be rendered in Project

---
+++ src/assets/utils/project-data.js
@@ -0,0 +1,57 @@
+import gymDiary from '../images/Gym-Diary.png';
+import blogme from '../images/blogme.png';
+import cinematic from '../images/CineMatic.png';
+import findYourForecase from '../images/Find-Your-Forecast.png';
+// import showWhatYouKnow from '../images/Show-What-You-Know.png';
+import underConstruction from '../images/under-construction.jpg';
+
+export const projectData = [
+  {
+    name: "Gym Diary",
+    description: "A Full-Stack Workout Tracking App",
+    technologies: "{SQL, MVC, ORM, API routing}",
+    repoLink: "https://github.com/ItzDerian/gym-diary",
+    deployedApp: "https://tranquil-reaches-54677.herokuapp.com",
+    img: gymDiary
+  },
+  {
+    name: "Blogme",
+    description: "A Full-Stack Blog",
+    technologies: "{SQL, MVC, ORM, API routing, Tempating}",
+    repoLink: "https://github.com/yogs0ddhoth/blogme",
+    deployedApp: "https://still-eyrie-26683.herokuapp.com/",
+    img: blogme
+  },
+  {
+    name: "CineMatic",
+    description: "A Movie Search Engine",
+    technologies: "[ES6, JQuery/AJAX, Web API, Bootstrap]",
+    repoLink: "https://github.com/tnoonan88/Cinematic-Film-Finder",
+    deployedApp: "https://tnoonan88.github.io/Cinematic-Film-Finder/",
+    img: cinematic
+  },
+  {
+    name: "Find Your Forecast",
+    description: "A Weather Forecast Application",
+    technologies: "[HTML, JQuery/AJAX, Bootstrap, CSS]",
+    repoLink: "https://github.com/yogs0ddhoth/find-your-forecast",
+    deployedApp: "https://yogs0ddhoth.github.io/find-your-forecast/",
+    img: findYourForecase
+  },
+  // {
+  //   name: "Show What You Know",
+  //   description: "A Timed Quiz on Javascript",
+  //   technologies: "[HTML, JS, Web API, CSS]",
+  //   repoLink: "",
+  //   deployedApp: "https://yogs0ddhoth.github.io/Show-what-you-know/",
+  //   img: showWhatYouKnow
+  // },
+  {
+    name: "Coming Soon!",
+    description: "",
+    technologies: "",
+    repoLink: "",
+    deployedApp: "",
+    img: underConstruction
+  },
+]
