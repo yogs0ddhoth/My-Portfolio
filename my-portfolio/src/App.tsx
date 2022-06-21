0b4539cdd457db307b0af99091cc6b71d4300897
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-21 16:57:03 -0400

    Header imported

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
