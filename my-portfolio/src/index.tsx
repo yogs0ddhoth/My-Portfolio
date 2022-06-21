ba1a0474e72e594ccd6787110a6c90f0d11f3645
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-21 17:17:05 -0400

    styling imported

--- my-portfolio/src/index.tsx
+++ my-portfolio/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
+import './assets/css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
