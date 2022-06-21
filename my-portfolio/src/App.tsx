992731a3232a2433080769077322082a99f44c64
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-21 17:40:53 -0400

    resume page imported

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -8,31 +8,20 @@ import './assets/css/style.css';
 import './assets/css/App.css';
 
 import Header from './components/Header';
-import Footer from './components/Footer';
 import About from './pages/About';
+import Resume from './pages/Resume';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App theme-1">
       <div className='contentWrap'>
         <Header />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
 
         <Routes>
           <Route path='/' element={<About />} />
+
+          <Route path='/resume' element={<Resume />} />
         </Routes>
         
       </div>
