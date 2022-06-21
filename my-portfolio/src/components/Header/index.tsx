c92e6fb77c7e0a8424e6f912f2eae3dd68b6f9e3
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-21 16:56:18 -0400

    componenet converted to typescript

---
+++ my-portfolio/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../../assets/css/style.css';
+import '../../assets/css/Header.css';
+
+import hero from "../../assets/images/marek-piwnicki-ibc616VrRo4-unsplash.jpg";
+
+import Navbar from './Navbar';
+
+export default function Header() {
+  return (
+    <div>
+      <img src={hero} alt="cover photo" className="hero"></img>
+      <header className='header theme-2 col-12 flex-row align-end pt-3'>
+        <NavLink to='/' className='home col-12 col-md-6'>
+          <h1 className='theme-2'>Ben Lin</h1>
+        </NavLink>
+        <Navbar />
+      </header>
+    </div>
+  )
+};
