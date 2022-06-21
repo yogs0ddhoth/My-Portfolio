8aeb12f3ca7bbd5eb860922e3dbfe92f7f8a5aca
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-21 17:00:27 -0400

    css imported

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -3,27 +3,32 @@ import { Routes, Route } from 'react-router-dom';
 
 import logo from './logo.svg';
 import './App.css';
+import './assets/css/index.css';
+import './assets/css/style.css';
+import './assets/css/App.css';
 
 import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App theme-1">
+      <div className='contentWrap'>
+        <Header />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     </div>
   );
 }
