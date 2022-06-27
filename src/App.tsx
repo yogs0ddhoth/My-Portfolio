e9a789816b06e90dd8fed6aed3714432d0e81125
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-27 16:33:51 -0400

    fontsource fonts imported and added to theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+
 import logo from './logo.svg';
 // import './App.css';
+
+import "@fontsource/londrina-sketch"
+import "@fontsource/average-sans";
+import theme from './utils/theme';
+
 import './assets/css/index.css';
 import './assets/css/style.css';
 import './assets/css/App.css';
 
 import {Header} from './layout/Header';
-import {About} from './pages/About';
+import About from './pages/About';
 import { Portfolio } from './pages/Portfolio';
 import {Contact} from './pages/Contact';
 import {Resume} from './pages/Resume';
 import {Footer} from './layout/Footer';
 
 export const App = () => (
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <div className="App theme-1">
       <div className='contentWrap'>
         <Header />
---
+++ src/utils/theme.tsx
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme(
+  {
+    fonts: {
+      heading: "'Londrina Sketch', 'cursive'",
+      mono: "'Average Sans', 'sans-serif'"
+    }
+  }
+)
+
+export default theme;
