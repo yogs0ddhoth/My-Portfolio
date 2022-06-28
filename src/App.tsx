1621d29c2caf2535640ba787619f5181f5578f1a
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-28 10:45:52 -0400

    mui themeprovider connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-
+import { ThemeProvider } from '@mui/material/styles';
 import logo from './logo.svg';
 // import './App.css';
 
 import "@fontsource/londrina-sketch"
 import "@fontsource/average-sans";
-import theme from './utils/theme';
+import { chakraTheme, muiTheme } from './utils/theme';
 
 import './assets/css/index.css';
 import './assets/css/style.css';
@@ -21,19 +21,23 @@ import {Resume} from './pages/Resume';
 import {Footer} from './layout/Footer';
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <div className="App theme-1">
-      <div className='contentWrap'>
-        <Header />
-        <Routes>
-          <Route path='/' element={<About />} />
-          <Route path='/portfolio' element={<Portfolio />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/resume' element={<Resume />} />
-        </Routes>
+  <ThemeProvider theme={muiTheme}>
+    <ChakraProvider theme={chakraTheme}>
+    
+      <div className="App theme-1">
+        <div className='contentWrap'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<About />} />
+            <Route path='/portfolio' element={<Portfolio />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/resume' element={<Resume />} />
+          </Routes>
 
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  </ChakraProvider>
+    
+    </ChakraProvider>
+  </ThemeProvider>
 );
