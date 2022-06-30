8e3e63eaca53cda1ef024b490d1ada5af490cc5a
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-30 14:34:59 -0400

    styliing updated

--- src/layout/Header/Navbar.tsx
+++ src/layout/Header/Navbar.tsx
@@ -2,32 +2,40 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Heading, Text } from '@chakra-ui/react';
 
-import '../../assets/css/style.css';
-import '../../assets/css/Navigation.css';
+// import '../../assets/css/style.css';
+// import '../../assets/css/Navigation.css';
 
 export const Navbar = () => (
-  <nav className='col-12 col-md-auto row align-end'>
+  // <nav className='col-12 col-md-auto row align-end'>
     <ul className='flex-row'>
       <li>
-        <NavLink to='/' className='theme-2'>
-          <Text as='h2' fontSize={'2xl'}>About</Text>
+        <NavLink to='/' className='theme-2 text-stone-50'>
+          <Text as='h2' fontSize={'2xl'}
+            textShadow='0 4px 4px rgb(0 0 0 / 0.95)'
+          >About</Text>
         </NavLink>
       </li>
       <li>
-        <NavLink to='/portfolio' className='theme-2'>
-          Portfolio
+        <NavLink to='/portfolio' className='theme-2 text-stone-50'>
+          <Text as='h2' fontSize={'2xl'}
+            textShadow='0 4px 4px rgb(0 0 0 / 0.95)'
+          >Portfolio</Text>
         </NavLink>
       </li>
       <li>
-        <NavLink to='/contact' className='theme-2'>
-          Contact
+        <NavLink to='/contact' className='theme-2 text-stone-50'>
+          <Text as='h2' fontSize={'2xl'}
+            textShadow='0 4px 4px rgb(0 0 0 / 0.95)'
+          >Contact</Text>
         </NavLink>
       </li>
       <li>
-        <NavLink to='/resume' className='theme-2'>
-          Resume
+        <NavLink to='/resume' className='theme-2 text-stone-50'>
+          <Text as='h2' fontSize={'2xl'}
+            textShadow='0 4px 4px rgb(0 0 0 / 0.95)'
+          >Resume</Text>
         </NavLink>
       </li>
     </ul>
-  </nav>
+  // </nav>
 );
