a0e4294e6dbece2aab9df5b92afae5e5bdfab937
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-30 21:28:47 -0400

    chakra components converted to material ui

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,30 +1,32 @@
 import React from 'react';
-import { Heading, Text } from '@chakra-ui/react';
+// import { Heading, Text } from '@chakra-ui/react';
 import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 // import avatar from '../images/avatar.png';
 // import '../assets/css/index.css';
 import '../assets/css/style.css';
 
 export default function About() {
-  const imgStyle = { borderRadius: '50%' };
+  
+  
 
   return (
     <div 
       // className='col-12 flex-row'
     >
-      <Heading as='h1' 
+      <Typography variant='h1' 
       // className='title col-12 align-start'
       >
         About Me
-      </Heading>
+      </Typography>
       {/* <img src={avatar} alt='Avatar' className='theme-1 col-3 col-2-md col-1-lg' style={imgStyle}/> */}
-      <Text 
+      <Typography variant='body1'
         // className='col-12'
       >
         Composer, Full Stack Web Developer; 
         <br />
         Crafting quality and intuitively coded applications.
-      </Text>
+      </Typography>
     </div>
   )
 };
