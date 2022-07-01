ae3075ef421e8c9150dd854e340244c0ca4e4b85
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-30 20:48:44 -0400

    component modularized and responsive

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -10,52 +10,50 @@ import { faLinkedin, faGithub, faStackOverflow } from '@fortawesome/free-brands-
 import '../../assets/css/style.css';
 import '../../assets/css/Footer.css';
 
-export const Footer = (props: {matchDownSm: boolean, matchUpLg: boolean}) => (
-  <div className='footer theme-2 col-12 pt-3'>
-    
-    {/* <ul className='col-12 flex-row'>
-      <li className="icon email col-2 col-1-sm row justify-center">
-        <a href="mailto:blin7.webdev@gmail.com" className='col-12 row'>
-          <FontAwesomeIcon className="theme-2 align-start fa-2x" icon={solid('envelope')} />
-        </a>
-      </li>
-      <li className="icon col-2 col-1-sm row justify-center">
-        <a href="https://github.com/yogs0ddhoth" className='col-12 row'>
-          <FontAwesomeIcon className="theme-2 align-start fa-2x" icon={faGithub} />
-        </a>
-      </li>
-      <li className="icon col-2 col-1-sm row justify-center">
-        <a href="https://stackoverflow.com/users/17608484/blin" className='col-12 row'>
-          <FontAwesomeIcon className="theme-2 align-start fa-2x" icon={faStackOverflow} />
-        </a>
-      </li>
-      <li className="icon col-2 col-1-sm row justify-center">
-        <a href="https://www.linkedin.com/in/ben-lin-b70a79227/" className='col-12 row'>
-          <FontAwesomeIcon className="theme-2 align-start fa-2x" icon={faLinkedin} />
-        </a>
-      </li>
-    </ul> */}
-
+const linksData = [
+  { href: "mailto:blin7.webdev@gmail.com", icon: solid('envelope')},
+  { href: "https://github.com/yogs0ddhoth", icon: faGithub },
+  { href: "https://stackoverflow.com/users/17608484/blin", icon: faStackOverflow },
+  { href: "https://www.linkedin.com/in/ben-lin-b70a79227/", icon: faLinkedin }
+]
+export default function Footer(props: {matchDownMd: boolean}) {
+  return (props.matchDownMd === true) ? (
+    <div className='footer theme-2 col-span-12 flex-row pt-3'>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={3}
+      >
+        {
+          linksData.map(link => (
+            <IconButton
+              LinkComponent={'a'}
+              href={link.href}
+            >
+              <FontAwesomeIcon className="fa-lg drop-shadow-icon" icon={link.icon} />
+            </IconButton>
+          ))
+        }
+      </Stack>
+    </div>
+    ) : (
     <Stack
       direction="column"
       justifyContent="center"
       alignItems="center"
       spacing={2}
     >
-      <IconButton
-        // onClick={}
-      >
-        <FontAwesomeIcon className="theme-2 fa-2x drop-shadow-icon" icon={solid('envelope')} />
-      </IconButton>
-      <IconButton>
-        <FontAwesomeIcon className="theme-2 fa-2x drop-shadow-icon" icon={faGithub} />
-      </IconButton>
-      <IconButton>
-        <FontAwesomeIcon className="theme-2 fa-2x drop-shadow-icon" icon={faStackOverflow} />
-      </IconButton>
-      <IconButton>
-        <FontAwesomeIcon className="theme-2 fa-2x drop-shadow-icon" icon={faLinkedin} />
-      </IconButton>
+      {
+        linksData.map(link => (
+          <IconButton
+            LinkComponent={'a'}
+            href={link.href}
+          >
+            <FontAwesomeIcon className="fa-lg drop-shadow-icon" icon={link.icon} />
+          </IconButton>
+        ))
+      }
     </Stack>
-  </div>
-);
+  )
+};
