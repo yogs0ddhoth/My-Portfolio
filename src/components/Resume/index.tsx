735a22d88e8468c6f12415ccdc8f690ffb2d1cd1
yogs0ddhoth <blin7.webdev@gmail.com>
2022-07-04 09:49:15 -0400

    skills tabs created

---
+++ src/components/Resume/index.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box 
+          sx={{ p: 3 }}
+        >
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+};
+
+const PanelList = ({list}: {list: string[]}) => {
+  return (
+    <List>
+      {list.map( (element) => {
+          return (
+            <ListItem>
+              <ListItemText>
+                {element}
+              </ListItemText>
+            </ListItem>
+          )
+        })
+      }
+    </List>
+  )
+};
+
+export default function VerticalTabs({matchDownSm}: {matchDownSm: boolean}) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box className='
+        h-full col-span-12
+        grid grid-cols-5 grid-rows-5
+      '
+      sx={{ 
+        flexGrow: 1, 
+        bgcolor: 'background.paper', 
+        display: 'flex', 
+        // height: 224 
+      }}
+    >
+    {/* <div className='pl-5 col-span-12 row-span-6'> */}
+      <Tabs 
+        className='col-span-5 md:col-span-1'
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ 
+          borderRight: 1, 
+          borderColor: 'divider' 
+        }}
+      >
+        <Tab label="Languages" {...a11yProps(0)} />
+        <Tab label="Frameworks" {...a11yProps(1)} />
+        <Tab label="Tools" {...a11yProps(2)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <PanelList 
+          list={["HTML5", "CSS3", "JavaScript/TypeScript", "SQL"]}
+        />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <PanelList 
+          list={["Node.js/Express.js", "React", "Webpack", "GraphQL"]}
+        />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <PanelList 
+          list={["MongoDB", "MySQL", "Github", "Heroku", "Web API"]}
+        />
+      </TabPanel>
+    </Box>
+  );
+}
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -5,51 +5,40 @@ import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import VerticalTabs from '../components/Resume'
 import benLinResume from '../assets/resume/Ben-Lin-Resume.pdf';
+
 // import '../assets/css/index.css';
 import '../assets/css/style.css';
 
-export const Resume = () => (
+export const Resume = ({matchDownSm}: {matchDownSm: boolean}) => (
   <Card className='
       opacity-95 drop-shadow-icon
       col-span-10
-      grid grid-cols-10 grid-rows-7 justify-center
+      grid grid-cols-12 grid-rows-6 justify-center
     '
   >
-    {/* <Typography variant='h1' 
-      className='col-span-12'
-    >
-      My Resume:<a href={'../assets/resume/Ben-Lin-Resume.pdf'} download={Resume} className=''>
-      download
-      </a>
-    </Typography> */}
-    <Typography variant='h1' 
-      className='col-span-10'
-    >
-      My Skills:
+    <Typography variant='h1' className='pl-3 col-span-12 row-span-1'>
+      Skills
     </Typography>
-    <a href={benLinResume} className='col-span-3' download={benLinResume}>
-      <Button variant="outlined" className='col-span-3'>
-        Download
-      </Button>
-    </a>
 
-    <div className='col-span-12 row-span-4 flex-row align-start'>
-      <Typography variant='h3' className='col-12'>
-        Proficiencies:
-      </Typography>
-      {/* <ul className='col-12 '>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>ES6+</li>
-        <li>SQL</li>
-        <li>Object Oriented Programing</li>
-        <li>Document Oriented Databases</li>
-        <li>GraphSQL</li>
-        <li>JS Libraries</li>
-        <li>React/JSX</li>
-        <li>MERN Stack</li>
-      </ul> */}
+    <div className='pl-5 pr-5 col-span-12 row-span-4'>
+      <VerticalTabs matchDownSm={matchDownSm} />
+    </div>
+
+    <div className='
+        pr-5 justify-items-end
+        col-span-12 row-span-1
+        flex justify-end items-center
+      '
+    >
+      <a href={benLinResume} download>
+        <Button variant="outlined" size='large'>
+          Download
+        </Button>
+      </a>
     </div>
   </Card>
 );
