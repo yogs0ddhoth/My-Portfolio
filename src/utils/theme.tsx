8deb747383d7ba7962dfef174d5eb37b1ec3c828
yogs0ddhoth <blin7.webdev@gmail.com>
2022-07-07 11:25:26 -0400

    chakra uninstalled

--- src/utils/theme.tsx
+++ src/utils/theme.tsx
@@ -1,17 +1,5 @@
-import { extendTheme } from "@chakra-ui/react";
 import { createTheme } from "@mui/material/styles";
 import { create } from "@mui/material/styles/createTransitions";
-
-export const chakraTheme = extendTheme(
-  {
-    fonts: {
-      heading: "'Londrina Sketch', 'cursive'",
-      // heading: "'Kolker Brush', 'cursive'",
-      // heading: "'Rock 3d', 'cursive'",
-      body: "'Average Sans', 'sans-serif'"
-    }
-  }
-);
 
 export const darkTheme = createTheme(
   {
