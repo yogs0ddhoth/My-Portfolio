0cd9b62812e5e9625bf8b215adaa56d69bbea7c4
yogs0ddhoth <blin7.webdev@gmail.com>
2022-10-09 23:02:38 -0400

    project component extracted

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -1,25 +1,85 @@
-import React, { useState }from 'react';
-import { ProjectType } from '../../assets/Types';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
 
-import { Card, Popover } from '@mui/material';
-// import '../../assets/css/index.css';
-// import '../../assets/css/style.css';
-import '../../assets/css/Project.css';
+import GitHub from '@mui/icons-material/GitHub'; // alternate repo link icon
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
-// export default function Project(props: ProjectType) {
-//   return (
-//     <div 
-//       // className='project col-12 col-md-5'
-//     >
-//       <a href={props.deployedApp} className=''>
-//         <img src={props.img} alt={props.description} />
-//       </a>
-//       <div>
-//         <h4>{props.name}</h4>
-//         <p>{props.description}</p>
-//         <p>{props.technologies}</p>
-//         <a href={props.repoLink}>Link to Repository</a>
-//       </div>
-//     </div>
-//   )
-// };
+import { Project } from '../../assets/project-data';
+import { ReactElement } from 'react';
+
+interface LinkTooltipProps {
+  href: string,
+  placement?: "bottom" | "bottom-end" | "bottom-start" | "left-end" | "left-start" | "left" | "right-end" | "right-start" | "right" | "top-end" | "top-start" | "top",
+  title: string,
+  children: ReactElement
+}
+
+export default function({
+  name,
+  description,
+  technologies,
+  repoLink,
+  deployedApp,
+  img
+}: Project) {
+  const LinkTooltip = ({
+    href, 
+    placement, 
+    title, 
+    children
+  }: LinkTooltipProps) => (
+    <Tooltip placement={placement ? placement : 'bottom'}
+      title={
+        <Link href={href} color='inherit'>
+          {title}
+        </Link>
+      }
+    >
+      {children}
+    </Tooltip>
+  )
+  return (
+    <ImageListItem key={name}>
+      <img alt={name}
+        src={`${img}?w=248&fit=crop&auto=format`}
+        srcSet={`${img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+      />
+
+      <ImageListItemBar
+        title={
+          <LinkTooltip href={deployedApp} 
+            title='Link to Deployed App' placement='bottom-start'
+          >
+            <Link variant='h5' color='inherit' 
+              href={deployedApp}
+            >
+              {name}
+            </Link>
+          </LinkTooltip>
+        }
+        subtitle={
+          <>
+            <Typography variant='subtitle1'>{description}</Typography>
+            <Typography variant='subtitle2' className='whitespace-normal'>
+              {technologies}
+            </Typography>
+          </>
+        }
+        actionIcon={
+          <LinkTooltip href={repoLink} 
+            title='Link to Repository' placement='bottom'
+          >
+            <IconButton href={repoLink}>
+              <OpenInNewIcon />
+            </IconButton>
+          </LinkTooltip>
+        }
+        actionPosition="right"
+      />
+    </ImageListItem>
+  )
+};
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,22 +1,23 @@
 import Card from '@mui/material/Card';
-import Typography from '@mui/material/Typography';
 
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
 
 import GitHub from '@mui/icons-material/GitHub';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import Project from '../components/Project';
 // import '../assets/css/style.css';
-import { projectData } from '../assets/project-data';
-import { Tooltip } from '@mui/material';
+import { Projects } from '../assets/project-data';
 
-export default function Portfolio(props: {matchDownMd: boolean, matchUpLg: boolean}) {
+export default function(props: {matchDownMd: boolean, matchUpLg: boolean}) {
 
-  console.log(projectData);
+  console.log(Projects);
   return (
-    <div className='col-span-12'>
+    // <div className='col-span-12'>
       <ImageList 
         variant="masonry" 
         cols={props.matchDownMd ? 1 : props.matchUpLg ? 3 : 2} 
@@ -25,76 +26,17 @@ export default function Portfolio(props: {matchDownMd: boolean, matchUpLg: boole
       >
         <ImageListItem key="Subheader" className='w-full'>
           <Card className='
-              opacity-95 drop-shadow-icon
-              flex flex-row justify-center
+            opacity-95 drop-shadow-icon
+            flex flex-row justify-center
             '
           >
-            <Typography variant='h2' >
-              My Work
-            </Typography>
+            <Typography variant='h2'>My Work</Typography>
           </Card>
         </ImageListItem>
-        {
-          projectData.map(project => (
-            <ImageListItem key={project.img}>
-              <img
-                src={`${project.img}?w=248&fit=crop&auto=format`}
-                srcSet={`${project.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                alt={project.name}
-              />
-              <ImageListItemBar
-                title={
-                  <Tooltip placement='bottom-start'
-                    title={
-                      <a href={project.deployedApp}
-                        className='hover:underline'
-                      >
-                        Link to Deployed App
-                      </a>
-                    }
-                  >
-                    <a href={project.deployedApp}>
-                      <Typography variant='h5'
-                        className='underline hover:text-pink-500'
-                      >
-                        {project.name}
-                      </Typography>
-                    </a>
-                  </Tooltip>
-                }
-                subtitle={
-                  <>
-                    <Typography variant='subtitle1'>{project.description}</Typography>
-                    <Typography variant='subtitle2' className='whitespace-normal'>{project.technologies}</Typography>
-                  </>
-                }
-                sx={{ whiteSpace: "normal"}}
-                actionIcon={
-                  <Tooltip placement='bottom'
-                    title={
-                      <a href={project.repoLink}
-                        className='hover:underline'
-                      >
-                        Link to Repository
-                      </a>
-                    }
-                  >
-                    <a href={project.repoLink}>
-                      <IconButton
-                        sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                        // aria-label={`info about ${project.title}`}
-                      >
-                        <OpenInNewIcon />
-                      </IconButton>
-                    </a>
-                  </Tooltip>
-                }
-                actionPosition="right"
-              />
-            </ImageListItem>
-          ))
-        }
+        {Projects.map(
+          project => <Project {...project} />
+        )}
       </ImageList>
-    </div>
+    // </div>
   )
 };
