20e087c7967e4067bcc9ee97058874d5b26ad66b
yogs0ddhoth <blin7.webdev@gmail.com>
2022-12-29 12:17:21 -0500

    resume data and dynamic type created

---
+++ src/assets/resume/index.d.ts
@@ -0,0 +1,11 @@
+export interface ResumeData {
+  languages: {
+    [index: string]: string[]
+  };
+  backend: {
+    [index: string]: string[]
+  };
+  frontend: {
+    [index: string]: string[]
+  };
+}
